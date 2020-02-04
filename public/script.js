2e3cbf30d7d7171a8368a1588d985f8ef34be6c1
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-02-05 00:14:01 +0100

    added background color change depending on weather condition feature, removed unnecessary block comment

--- public/script.js
+++ public/script.js
@@ -90,7 +90,7 @@ searchBox.addEventListener('keypress', (event) => {
     }
 })
 
-/*
+/* replacement code if google places api is being used (needs to be adjusted though)
 searchBox.addListener('places_changed', () => {
     const place = searchBox.getPlaces()[0]
     if (place == null) return
@@ -111,19 +111,4 @@ searchBox.addListener('places_changed', () => {
         setWeatherData(data, place.formatted_address)
     })
 })
-*/
-
-/*
-{"coord":{"lon":-0.13,"lat":51.51},
-"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],
-"base":"stations",
-"main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},
-"visibility":10000,
-"wind":{"speed":4.1,"deg":80},
-"clouds":{"all":90},
-"dt":1485789600,
-"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},
-"id":2643743,
-"name":"London",
-"cod":200}
 */
