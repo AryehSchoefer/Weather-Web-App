644a6676acb170a7e3bf2e9f27a393122502113d
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-02-05 00:12:44 +0100

    added background color change depending on weather condition feature, removed unnecessary comments

--- public/script.js
+++ public/script.js
@@ -1,8 +1,21 @@
+const backgroundStyles = {
+    'clear-day': 'linear-gradient(120deg, #f6d365 0%, #fda085 100%)',
+    'clear-night': 'linear-gradient(to top, #30cfd0 0%, #330867 100%)',
+    'partly-cloudy-day': 'linear-gradient(to top, #37ecba 0%, #72afd3 100%)',
+    'partly-cloud-night': 'linear-gradient(to top, #5ee7df 0%, #b490ca 100%)',
+    'cloudy': 'linear-gradient(60deg, #29323c 0%, #485563 100%)',
+    'rain': 'linear-gradient(rgb(25, 27, 42), rgb(86, 93, 145))',
+    'snow': 'linear-gradient(to right, #243949 0%, #517fa4 100%)',
+    'sleet': 'linear-gradient(to right, #868f96 0%, #596164 100%)',
+    'wind': 'linear-gradient(-20deg, #616161 0%, #9bc5c3 100%)',
+    'fog': 'linear-gradient(180deg, rgba(18,21,23,1) 0%, rgba(94,95,98,1) 100%)'
+}
 const apiBase = 'api.openweathermap.org/data/2.5/'
 const API_KEY = '9081c26a42e71c0b2ecd31972a2c36ba'
 const proxy = 'https://cors-anywhere.herokuapp.com/' // for localhost use
 //const icon = document.querySelector('.icon')
 const icon = new Skycons({'color': 'white'})
+const body = document.body;
 const temperatureDegrees = document.querySelector('.temperature-degrees')
 const apparentTemperature = document.querySelector('.apparent-temperature')
 const weatherDescription = document.querySelector('.weather-description')
@@ -48,11 +61,23 @@ function setWeatherData(data) {
     apparentTemperature.textContent = `Feels like ${Math.round(data.apparentTemperature)}°C.`
     weatherDescription.textContent = `${data.summary}.`
     searchBox.value = ''
+
     icon.set('icon', data.icon)
     icon.play()
+
+    body.style.background = backgroundStyles[data.icon]
+    body.style.backgroundAttachment = 'fixed'
+
+    // kinda performance intensive
+    if (data.icon == 'rain') {
+        body.classList.add('weather', 'rain')
+    }
+    if (data.icon == 'snow') {
+        body.classList.add('weather', 'snow')
+    }
 }
 
-icon.set('icon', 'clear-day')
+icon.set('icon', 'CLEAR_DAY')
 icon.play()
 
 searchBox.addEventListener('keypress', (event) => {
