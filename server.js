00a13533aefe0e2267753f0ec4ed317404243269
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-07-20 17:07:12 +0200

    made deploy ready

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.post('/weather', (req, res) => {
     }).then(data => res.json(data.data.currently))
 })
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     console.log('Server Started')
 })
